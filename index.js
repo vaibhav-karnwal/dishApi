b7e192932082aacd9bde500b7fb09821e746ab38
Vaibhav Karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2021-07-18 23:43:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
-
+const dishData = require("./dish.json");
 
 app.get("/", (req,res)=>{
     res.send("Hello World");
+});
+
+app.get('/api/books', (req,res)=> {
+    res.send(dishData);
 });
 
 app.listen(port, ()=>{
