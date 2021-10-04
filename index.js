dc5dc9248d0c294f3887036656e03ff6855fc822
Vaibhav Karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2021-10-05 01:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express();
 let port = process.env.PORT || 3000;
 const dishData = require("./dish.json");
 const projectData = require("./projectApi.json");
-
+const detailData = require("./detail.json");
 app.get("/", (req,res)=>{
     res.send("Hello World");
 });
 
 app.get('/dish', (req,res)=> {
     res.send(dishData);
+});
+
+app.get('/dish', (req,res)=> {
+    res.send(detailData);
 });
 
 app.get('/project', (req,res)=> {
